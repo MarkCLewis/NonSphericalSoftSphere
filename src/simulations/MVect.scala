ad065ccc0e2de941e8116ad9213386258e3f8172
Reidlee Lynn <rlynn1@trinity.edu>
2017-06-14 10:37:40 -0700

    blah

---
+++ src/simulations/MVect.scala
@@ -0,0 +1,224 @@
+package simulations
+import Math.sqrt
+
+class MVect3(var x: Double, var y: Double, var z: Double) {
+
+  def zero(): Unit = {
+    x = 0.0
+    y = 0.0
+    z = 0.0
+  }
+
+  def dot(m: MVect3): Double = {
+    x * m.x + y * m.y + z * m.z
+  }
+
+  def normalize() {
+    val mag = sqrt(this dot this)
+    x /= mag
+    y /= mag
+    z /= mag
+  }
+
+  def -(m: MVect3): MVect3 = new MVect3(x - m.x, y - m.y, z - m.z)
+
+  def mag() = {
+    sqrt(this dot this)
+  }
+}
+
+class MutableBody(
+  val p: MVect3,
+  val v: MVect3,
+  val mass: Double,
+  val radius: Double)
+
+class NBodyMutableClass(val dt: Double) {
+  var cutoff = 0.8
+  var sd = new MVect3(1, 0, 0) // softness direction
+  var soft = 0.9
+  var b1 = 100.0;
+
+  var bodies = Array(
+    new MutableBody(new MVect3(0, 5, 0), new MVect3(0, 0, 0), 1e-10, 1),
+    new MutableBody(new MVect3(0, 1, 0), new MVect3(0, 0, 0), 1e-10, 1))
+
+  var numBodies = bodies.length
+
+  def setB(b: Double) {
+    b1 = b
+  }
+
+  def setBodies(bods: Array[(Double, Double)]) {
+    bodies = Array()
+    for (b <- bods) {
+      bodies :+ new MutableBody(new MVect3(b._1, b._2, 0), new MVect3(0, 0, 0), 1e-10, 1)
+    }
+    numBodies = bodies.length
+  }
+
+  def setSD(x: Double, y: Double) {
+    sd = new MVect3(x, y, 0)
+  }
+
+  def randomBodies(num: Int) {
+    bodies = Array.fill(num) {
+      val r = math.random * num * 5 + 2
+      val theta = (math.random - 0.5) * 1
+      new MutableBody(new MVect3(r * math.sin(theta), r * math.cos(theta), 0), new MVect3(0, 0, 0), 1e-10, 1)
+    }
+  }
+
+  def gridBodies() {
+    bodies = (Array.tabulate(7, 7) { (i, j) =>
+      new MutableBody(new MVect3(i * 3 + math.random * 0.5, j * 3 + 2, 0), new MVect3(0, 0, 0), 1e-10, 1)
+    }).flatten
+  }
+
+  private val accel = Array.fill(numBodies)(new MVect3(0, 0, 0))
+  private var time = 0.0
+
+  //  initBodies()
+  //  def initBodies(): Unit = {
+  //    bodies(0).mass = 1.0
+  //    for (i <- 1 until numBodies) {
+  //      bodies(i).p.x = i
+  //      bodies(i).v.y = math.sqrt(1.0 / i)
+  //    }
+  //  }
+
+  def printBodies() {
+    for (b <- bodies) {
+      println(s"$b1 $time ${b.p.x} ${b.p.y} ${b.p.z} ${b.v.x} ${b.v.y} ${b.v.z}")
+    }
+  }
+
+  def forSim(steps: Int, collideFunc: (Int, Int) => Unit): Double = {
+    var maxAccel = 0.0
+    for (_ <- 1 to steps) {
+
+      for (i <- 0 until numBodies) {
+        accel(i).zero()
+        constantAccel(i, new MVect3(0, -1, 0))
+
+        val p = new MVect3(0, 1, 0)
+        planeBounce(i, p, 0)
+        planeBounce(i, new MVect3(1, 0, 0), -50)
+        planeBounce(i, new MVect3(-1, 0, 0), -50)
+
+        //        val p1 = new MVect3(1 / sqrt(2), 1 / sqrt(2), 0)
+        //        val p2 = new MVect3(1 / (-sqrt(2)), 1 / sqrt(2), 0)
+        //        planeBounce(i, p1, 0)
+        //        planeBounce(i, p2, 0)
+
+      }
+
+      for {
+        i <- 0 until numBodies
+        j <- i + 1 until numBodies
+      } {
+        //gravityForce(i,j)
+        collideFunc(i, j)
+      }
+      for (i <- 0 until numBodies) {
+        val p = bodies(i)
+        val a = accel(i).mag()
+        if (a > maxAccel) maxAccel = a
+        p.v.x += accel(i).x * dt
+        p.v.y += accel(i).y * dt
+        p.v.z += accel(i).z * dt
+        p.p.x += p.v.x * dt
+        p.p.y += p.v.y * dt
+        p.p.z += p.v.z * dt
+      }
+      time += dt
+    }
+    maxAccel
+  }
+
+  def f(x: Double): Double = (x - cutoff) / (1 - cutoff) // fixxxxxxxxxxx
+
+  def softCollide(i: Int, j: Int) {
+    val pi = bodies(i)
+    val pj = bodies(j)
+    val dx = pi.p.x - pj.p.x
+    val dy = pi.p.y - pj.p.y
+    val dz = pi.p.z - pj.p.z
+    val dist = math.sqrt(dx * dx + dy * dy + dz * dz)
+    if (dist < (pi.radius + pj.radius)) {
+      val overlap = (pi.radius + pj.radius) - dist
+      val n = new MVect3(dx, dy, dz)
+      n.normalize
+      val softFactor = sd dot n
+      val softB = if (softFactor > cutoff) b1 * (1 - soft * f(softFactor)) else b1
+
+      val v = -(n dot (pi.v - pj.v))
+      val mag = -softB * overlap - v * 10
+      accel(i).x -= n.x * mag //need to divide by mass
+      accel(i).y -= n.y * mag
+      accel(i).z -= n.z * mag
+      accel(j).x += n.x * mag
+      accel(j).y += n.y * mag
+      accel(j).z += n.z * mag
+    }
+  }
+
+  def collide(i: Int, j: Int) {
+    val pi = bodies(i)
+    val pj = bodies(j)
+    val dx = pi.p.x - pj.p.x
+    val dy = pi.p.y - pj.p.y
+    val dz = pi.p.z - pj.p.z
+    val dist = math.sqrt(dx * dx + dy * dy + dz * dz)
+    if (dist < (pi.radius + pj.radius)) {
+      val overlap = (pi.radius + pj.radius) - dist
+      val n = new MVect3(dx, dy, dz)
+      n.normalize
+      val v = -(n dot (pi.v - pj.v))
+      val mag = -b1 * overlap - v * 10
+      accel(i).x -= n.x * mag //need to divide by mass
+      accel(i).y -= n.y * mag
+      accel(i).z -= n.z * mag
+      accel(j).x += n.x * mag
+      accel(j).y += n.y * mag
+      accel(j).z += n.z * mag
+    }
+  }
+
+  def gravityForce(i: Int, j: Int) {
+    val pi = bodies(i)
+    val pj = bodies(j)
+    val dx = pi.p.x - pj.p.x
+    val dy = pi.p.y - pj.p.y
+    val dz = pi.p.z - pj.p.z
+    val dist = math.sqrt(dx * dx + dy * dy + dz * dz)
+    val magi = pj.mass / (dist * dist * dist)
+    accel(i).x -= magi * dx
+    accel(i).y -= magi * dy
+    accel(i).z -= magi * dz
+    val magj = pi.mass / (dist * dist * dist)
+    accel(j).x += magj * dx
+    accel(j).y += magj * dy
+    accel(j).z += magj * dz
+  }
+
+  def constantAccel(i: Int, g: MVect3) {
+    accel(i).x += g.x
+    accel(i).y += g.y
+    accel(i).z += g.z
+  }
+
+  def planeBounce(i: Int, n: MVect3, d: Double) {
+    val pi = bodies(i)
+    val dist = (n dot pi.p) - d
+    if (dist < pi.radius) {
+      val overlap = pi.radius - dist
+      val v = -(n dot pi.v)
+      val mag = b1 * overlap + v * 10
+      //val mag = 50 * overlap + v * 20
+      accel(i).x += n.x * mag
+      accel(i).y += n.y * mag
+      accel(i).z += n.z * mag
+    }
+  }
+}
---
+++ src/simulations/Main.scala
@@ -0,0 +1,105 @@
+package simulations
+
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.canvas.Canvas
+import scalafx.animation.AnimationTimer
+
+//Tests softness of different x, y orientations of the soft spot
+//object Main extends App {
+//
+//  val vals = for (t <- 0.0 until math.Pi by 0.01) yield {
+//    (math.cos(t), math.sin(t))
+//  }
+//
+//  for ((x, y) <- vals) {
+//    var sim = new NBodyMutableClass(0.0001)
+//    sim.setSD(x, y)
+//    var flag = true
+//    while (flag) {
+//      val maxAccel = sim.forSim(100, sim.softCollide)
+//      //val maxAccel = sim.forSim(100,sim.collide)
+//      //println(maxAccel)
+//      if (maxAccel < 1e-4) {
+//        flag = false
+//        sim.printBodies()
+//      }
+//    }
+//  }
+//
+//}
+
+//object Main extends App {
+//  val values = Array(1000, 500, 400, 300, 250, 200, 150, 100, 50, 25, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+//  //val values = Array(100)
+//  for (v <- values) {
+//    var sim = new NBodyMutableClass(0.0001, v) // v is b1
+//    var flag = true
+//    while (flag) {
+//      //for (_ <- 1 to 1000) {
+//      val maxAccel = sim.forSim(100, sim.softCollide)
+//      //println(maxAccel)
+//      if (maxAccel < 1e-4) {
+//        flag = false
+//        sim.printBodies()
+//      }
+//    }
+//  }
+//}
+
+object Main extends JFXApp {
+  val sim = new NBodyMutableClass(0.0001)
+  //sim.stackBodies()
+  //sim.randomBodies(10)
+
+  stage = new JFXApp.PrimaryStage {
+    title = "Particle stuff"
+    scene = new Scene(800, 800) {
+      val canvas = new Canvas(800, 800)
+      content = canvas
+      val gc = canvas.graphicsContext2D
+      gc.translate(400, 400)
+      gc.scale(5, -5)
+      val timer: AnimationTimer = AnimationTimer { time =>
+        sim.forSim(100, sim.softCollide)
+        gc.clearRect(-100, -100, 200, 200)
+        //        gc.strokeLine(-100,-100,100,100)
+        //        gc.strokeLine(-100,100,100,-100)
+        for (body <- sim.bodies) {
+          gc.fillOval(body.p.x - 1, body.p.y - 1, 2, 2)
+        }
+      }
+      timer.start
+    }
+  }
+}
+
+
+// sd = softness direction, cutoff, and soft = softness factor
+
+// this if the value that will replace b1... "soft b1"
+// if (sd dot n > cutoff) {
+// b1 * ( 1 - soft * f(sd))
+// } else { b1 }
+
+// f(x) = (x - cutoff) / (1 - cutoff)
+
+/* 
+ * Timestep = 0.000001
+ * steps = 10000
+ * 
+ * b1 = 1000
+ * 6.290000000525299 0.0 2.9980008704216035 0.0 0.0 -1.2451621835194464E-5 0.0
+ * 6.290000000525299 0.0 0.9979988687751139 0.0 0.0 -2.9970817137476443E-5 0.0
+ * 
+ * b1 = 500
+ * 6.260000000521106 0.0 2.996003570237496 0.0 0.0 -3.135273724638581E-4 0.0
+ * 6.260000000521106 0.0 0.9960002310429089 0.0 0.0 2.792753758878369E-4 0.0
+ * 
+ * b1 = 100
+ * 4.920000000333803 0.0 2.9800002929436498 0.0 0.0 9.353180473816068E-5 0.0
+ * 4.920000000333803 0.0 0.9800002137323449 0.0 0.0 -9.201680930065943E-5 0.0
+ * 
+ * b1 = 50
+ * -- not possible -- gains too much energy at 50 ???
+ */
